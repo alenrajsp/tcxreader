caa291f894eab1c28862258259869e0b4a4d01bd
alenrajsp <alen.rajsp@um.si>
2020-12-02 23:32:41 +0100

    Updated missed version update!

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tcxreader",  # Replace with your own username
-    version="0.2.1",
+    version="0.3.0",
     author="Alen Rajšp",
     author_email="alen.rajsp@gmail.com",
     description="tcxreader is a parser/reader for Garmin’s TCX file format. It also works well with missing data!",
