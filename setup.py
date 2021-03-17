1c89cb53f9cbbb06cb6f21d4587252691e48f877
iztokf <iztok@iztok-jr-fister.eu>
2021-03-17 22:05:32 +0100

    include data for tests

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/alenrajsp/tcxreader",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         "maya"
     ],
