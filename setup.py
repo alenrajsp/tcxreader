b9826a208fdbb404938221cde88f062be3721bd4
alenrajsp <alen.rajsp@um.si>
2023-01-14 14:25:35 +0100

    Reduced package size
    Greatly reduced package size by removing the following directories from pypi release: examples, example_data, tests

--- setup.py
+++ setup.py
@@ -6,14 +6,18 @@ with open("README.md", encoding='UTF-8', mode="r") as fh:
 
 setuptools.setup(
     name="tcxreader",
-    version="0.4.3",
+    version="0.4.4",
     author="Alen Rajšp",
     author_email="alen.rajsp@gmail.com",
     description="tcxreader is a reader for Garmin’s TCX file format. It also works well with missing data!",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/alenrajsp/tcxreader",
-    packages=setuptools.find_packages(exclude=['*tests*']),
+    packages=setuptools.find_packages(exclude=("*.tests", "*.tests.*", "tests.*", "tests", '*tests*',
+                                                "*tcxreader.tests", "*.tcxreader.tests.*", "tcxreader.tests.*",
+                                               "tcxreader.tests", '*tcxreader.tests*',
+                                               "*.examples","*.examples.*", "examples.*", "examples",
+                                               "*.example_data", "*.example_data.*", "example_data.*", "example_data")),
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
